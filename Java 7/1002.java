9f8c47f0b7313ee0e573626857900133537338b7
Moutella <joaogabrielmsc@gmail.com>
2016-05-01 23:47:12 +0000

    Desafio 1002

---
+++ Java 7/1002.java
@@ -0,0 +1,24 @@
+//Nome: Área do Círculo
+//Resultado: Compilation error
+//Data: 01/05/16 23:47:12
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        double d = sc.nextDouble();
+        double r = d * d * 3.14159;
+        System.out.printf("A=%.4f", r,"/n");
+    }
+ 
+}
