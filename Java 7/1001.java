828eaca13f720473a24bf977bf6dceee50e5aed4
Moutella <joaogabrielmsc@gmail.com>
2016-05-01 23:10:46 +0000

    Desafio 1001

---
+++ Java 7/1001.java
@@ -0,0 +1,23 @@
+//Nome: Extremamente Básico
+//Resultado: Compilation error
+//Data: 01/05/16 23:10:46
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.read
+        int b = sc.read
+        int x = a+b;
+        System.out.println("X = " + x); 
+    }
+ 
+}
