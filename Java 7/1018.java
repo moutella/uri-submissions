4f3973d2e09022375b1aa015552280df333c8af5
Moutella <joaogabrielmsc@gmail.com>
2016-05-02 21:18:18 +0000

    Desafio 1018

--- Java 7/1018.java
+++ Java 7/1018.java
@@ -1,6 +1,6 @@
 //Nome: Cédulas
-//Resultado: Presentation error
-//Data: 02/05/16 21:13:58
+//Resultado: Accepted
+//Data: 02/05/16 21:18:18
 //Linguagem: Java 7
 import java.io.IOException;
 import java.util.Scanner;
@@ -20,24 +20,24 @@ public class Main {
             int qnt100 = 0,qnt50 =0,qnt20=0,qnt10=0,qnt5=0,qnt2=0,qnt1=0;
             qnt100 = qnt/100;
             qnt -= qnt100*100;
-            System.out.println(qnt100 + " nota(s) de R$100,00");
+            System.out.println(qnt100 + " nota(s) de R$ 100,00");
             qnt50 = qnt/50;
             qnt -= qnt50*50;
-            System.out.println(qnt50 + " nota(s) de R$50,00");
+            System.out.println(qnt50 + " nota(s) de R$ 50,00");
             qnt20 = qnt/20;
             qnt -= qnt20*20;
-            System.out.println(qnt20 + " nota(s) de R$20,00");
+            System.out.println(qnt20 + " nota(s) de R$ 20,00");
             qnt10 = qnt/10;
             qnt -= qnt10*10;
-            System.out.println(qnt10 + " nota(s) de R$10,00");
+            System.out.println(qnt10 + " nota(s) de R$ 10,00");
             qnt5 = qnt/5;
             qnt -= qnt5*5;
-            System.out.println(qnt5 + " nota(s) de R$5,00");
+            System.out.println(qnt5 + " nota(s) de R$ 5,00");
             qnt2 = qnt/2;
             qnt -= qnt2*2;
-            System.out.println(qnt2 + " nota(s) de R$2,00");
+            System.out.println(qnt2 + " nota(s) de R$ 2,00");
             qnt1 = qnt;
-            System.out.println(qnt1 + " nota(s) de R$1,00");
+            System.out.println(qnt1 + " nota(s) de R$ 1,00");
     }
  
 }
