4ab4d22181d35ed208eb2ffe0d43a3fa47995988
Moutella <joaogabrielmsc@gmail.com>
2016-05-02 00:06:15 +0000

    Desafio 1005

--- Java 7/1005.java
+++ Java 7/1005.java
@@ -1,6 +1,6 @@
 //Nome: Média 1
-//Resultado: Presentation error
-//Data: 02/05/16 00:05:01
+//Resultado: Accepted
+//Data: 02/05/16 00:06:15
 //Linguagem: Java 7
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,7 +12,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble() * 3.5;
         double b = sc.nextDouble() * 7.5;
-        System.out.printf("MEDIA = %.5f \n", (a+b)/11);
+        System.out.printf("MEDIA = %.5f\n", (a+b)/11);
  
     }
  
