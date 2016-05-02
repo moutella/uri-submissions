4a95fcac21a975d80145e973367a4c3251667795
Moutella <joaogabrielmsc@gmail.com>
2016-05-02 23:23:48 +0000

    Desafio 1044

---
+++ Java 7/1044.java
@@ -0,0 +1,30 @@
+//Nome: Múltiplos
+//Resultado: Wrong answer (100%)
+//Data: 02/05/16 23:23:48
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b){
+            if(a%b==0)
+                System.out.println("Sao Multiplos");
+        }
+        else
+            if(b%a==0)
+                System.out.println("Sao Multiplos");
+        else
+                System.out.println("Nao sao Multiplos");
+    }
+ 
+}
