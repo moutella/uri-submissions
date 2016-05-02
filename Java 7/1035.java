07f29beed2314d69437db95d3f2f826b8d02e89a
Moutella <joaogabrielmsc@gmail.com>
2016-05-02 21:31:59 +0000

    Desafio 1035

---
+++ Java 7/1035.java
@@ -0,0 +1,28 @@
+//Nome: Teste de Seleção 1
+//Resultado: Accepted
+//Data: 02/05/16 21:31:59
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        if((b>c)&(d>a)&((c+d)>(a+b))&(c>=0)&(d>=0)&(a%2==0))
+            System.out.println("Valores aceitos");
+        else
+            System.out.println("Valores nao aceitos");
+    }
+ 
+}
