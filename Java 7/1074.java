b116f24c735674dc6d96e8a822df184b02fa1375
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 17:45:21 +0000

    Desafio 1074

---
+++ Java 7/1074.java
@@ -0,0 +1,32 @@
+//Nome: Par ou Ímpar
+//Resultado: Presentation error
+//Data: 03/05/16 17:45:21
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                System.out.println("NULL");
+            } else {
+                if (x % 2 == 0) {
+                    System.out.printf("EVEN ");
+                } else {
+                    System.out.printf("ODD ");
+                }
+            if (x > 0) {
+                System.out.printf("POSITIVE \n");
+            } else {
+                System.out.printf("NEGATIVE \n");
+            }
+            }
+        }
+    }
+ 
+}
