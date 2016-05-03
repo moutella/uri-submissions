7a327c5fbb0213dc83dca531a4493c7fd0625efb
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 20:19:34 +0000

    Desafio 1042

--- Java 7/1042.java
+++ Java 7/1042.java
@@ -1,9 +1,10 @@
 //Nome: Sort Simples
-//Resultado: Wrong answer (20%)
-//Data: 02/05/16 22:38:49
+//Resultado: Accepted
+//Data: 03/05/16 20:19:34
 //Linguagem: Java 7
 import java.io.IOException;
 import java.util.Scanner;
+ 
 /**
  * IMPORTANT: 
  *      O nome da classe deve ser "Main" para que a sua solução execute
@@ -17,41 +18,27 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if(a<b){
-            if(b<c){
-                System.out.println(a);
-                System.out.println(b);
-                System.out.println(c);
-            }
-            if(c<a){
-                System.out.println(c);
-                System.out.println(a);
-                System.out.println(b);
-            }
-            else{
-                System.out.println(a);
-                System.out.println(c);
-                System.out.println(b);
-            }
+        int aux;
+        int ord1 = a, ord2 = b, ord3 = c;
+        if (ord2 < ord1) {
+            ord1 = b;
+            ord2 = a;
         }
-        if(b<a){
-            if(a<c){
-                System.out.println(b);
-                System.out.println(a);
-                System.out.println(c);
-            }
-            if(c<b){
-                System.out.println(c);
-                System.out.println(b);
-                System.out.println(a);
-            }
-            else{
-                System.out.println(b);
-                System.out.println(c);
-                System.out.println(a);
-            }
+        if (ord3 < ord1) {
+            aux = ord1;
+            ord1 = ord3;
+            ord3 = ord2;
+            ord2 = aux;
         }
-        System.out.println("");
+        if (ord3 < ord2) {
+            aux = ord2;
+            ord2 = ord3;
+            ord3 = aux;
+        }
+        System.out.println(ord1);
+        System.out.println(ord2);
+        System.out.println(ord3);
+        System.out.println();
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
