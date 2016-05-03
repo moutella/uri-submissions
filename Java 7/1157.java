fd712363a308b916b5abbe7703a5e5b036007e6f
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 19:43:22 +0000

    Desafio 1157

---
+++ Java 7/1157.java
@@ -0,0 +1,26 @@
+//Nome: Divisores I
+//Resultado: Accepted
+//Data: 03/05/16 19:43:22
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+ 
+}
