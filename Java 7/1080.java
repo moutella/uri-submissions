a95d9307877365601804f98e02dca5d5867077ca
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 17:57:16 +0000

    Desafio 1080

--- Java 7/1080.java
+++ Java 7/1080.java
@@ -1,9 +1,10 @@
 //Nome: Maior e Posição
-//Resultado: Wrong answer (30%)
-//Data: 03/05/16 17:50:37
+//Resultado: Wrong answer (50%)
+//Data: 03/05/16 17:57:16
 //Linguagem: Java 7
 import java.io.IOException;
 import java.util.Scanner;
+ 
 /**
  * IMPORTANT: 
  *      O nome da classe deve ser "Main" para que a sua solução execute
@@ -14,23 +15,16 @@ public class Main {
  
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int maior = 0, menor = 0;
-        for (int i = 0; i < 100; i++) {
+        int maior = 0, pos = 0;
+        for (int i = 0; i < 10; i++) {
             int x = sc.nextInt();
-            if(i==0){
-                maior = x;
-                menor = x;
-            }
             if(x>maior){
                 maior = x;
-            }
-            if(x<menor){
-                menor = x;
+                pos = i+1;
             }
         }
         System.out.println(maior);
-        System.out.println(menor);
- 
+        System.out.println(pos);
     }
  
 }
