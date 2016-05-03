3d699c7d002b726d1ba347f3e1cf9e809aad4573
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 19:35:59 +0000

    Desafio 1115

--- Java 7/1115.java
+++ Java 7/1115.java
@@ -1,16 +1,10 @@
 //Nome: Quadrante
-//Resultado: Wrong answer (40%)
-//Data: 03/05/16 19:32:14
+//Resultado: Accepted
+//Data: 03/05/16 19:35:59
 //Linguagem: Java 7
 import java.io.IOException;
 import java.util.Scanner;
  
-/**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
 public class Main {
  
     public static void main(String[] args) throws IOException {
@@ -26,12 +20,12 @@ public class Main {
                 if (y > 0) {
                     System.out.println("primeiro");
                 } else {
-                    System.out.println("segundo");
+                    System.out.println("quarto");
                 }
             } else if (y < 0) {
                 System.out.println("terceiro");
             } else {
-                System.out.println("quarto");
+                System.out.println("segundo");
             }
 
         }
