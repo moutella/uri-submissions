91ed45055b629a894cd8e556a227efccb84e5836
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 17:35:21 +0000

    Desafio 1071

---
+++ Java 7/1071.java
@@ -0,0 +1,31 @@
+//Nome: Soma de Impares Consecutivos I
+//Resultado: Accepted
+//Data: 03/05/16 17:35:21
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n2 = sc.nextInt();
+        int aux;
+        if(n2>n){
+            aux = n;
+            n = n2;
+            n2 = aux;
+        }
+        if(n2%2==0)
+            n2+=1;
+        else
+            n2+=2;
+        aux = 0;
+        for (int i = n2; i < n; i+=2) {
+            aux += i;
+        }
+        System.out.println(aux);
+    }
+ 
+}
