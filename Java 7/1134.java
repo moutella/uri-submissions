80fe2d3b0d07937e393a37439a648eef4444596d
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 19:39:24 +0000

    Desafio 1134

---
+++ Java 7/1134.java
@@ -0,0 +1,35 @@
+//Nome: Tipo de Combustível
+//Resultado: Accepted
+//Data: 03/05/16 19:39:24
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        int alc=0,gas=0,die=0;
+        while (loop) {
+            int cod = sc.nextInt();
+            if (cod == 4){
+                System.out.println("MUITO OBRIGADO");
+                System.out.println("Alcool: "+alc);
+                System.out.println("Gasolina: "+gas);
+                System.out.println("Diesel: "+die);
+                    break;
+                }
+            if(cod==1){
+                alc++;
+            }
+            if(cod==2){
+                gas++;
+            }
+            if(cod==3){
+                die++;
+            }
+        }
+    }
+ 
+}
