100e75ffb246d43b7352625f76cfdb78386ca97f
Moutella <joaogabrielmsc@gmail.com>
2016-05-03 19:41:49 +0000

    Desafio 1153

---
+++ Java 7/1153.java
@@ -0,0 +1,27 @@
+//Nome: Fatorial Simples
+//Resultado: Accepted
+//Data: 03/05/16 19:41:49
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fat = 1;
+        for(int i = n;i>0;i--){
+            fat*=i;
+        }
+        System.out.println(fat);
+    }
+    
+}
+ 
