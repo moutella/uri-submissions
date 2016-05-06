92d32d58f56f89799309aa94d1d874990539218e
Moutella <joaogabrielmsc@gmail.com>
2016-05-06 23:42:03 +0000

    Desafio 1010

---
+++ Java 7/1010.java
@@ -0,0 +1,29 @@
+//Nome: Cálculo Simples
+//Resultado: Accepted
+//Data: 06/05/16 23:42:03
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int cod = 0, qnt1 = 0;
+        double precoTotal=0, preco=0;
+        for(int i = 0; i<2;i++){
+            cod=sc.nextInt();
+            qnt1=sc.nextInt();
+            preco=sc.nextDouble();
+            precoTotal+=preco*qnt1;
+        }
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", precoTotal);
+    }
+ 
+}
