66b3ba0e48b9c97336a3319fd244e6ccbe3e00f4
Moutella <joaogabrielmsc@gmail.com>
2016-05-06 15:07:46 +0000

    Desafio 1026

---
+++ Java 7/1026.java
@@ -0,0 +1,28 @@
+//Nome: Carrega ou não Carrega?
+//Resultado: Compilation error
+//Data: 06/05/16 15:07:46
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a, b;
+        do {
+            a = sc.nextLong();
+            b = sc.nextLong();
+            sc.nextLine();
+            System.out.println(a ^ b);
+        } while (sc.hasNextInt());
+    }
+ 
+}
