f3897234804b66c7639573a5cc2a41dd04ff8093
Moutella <joaogabrielmsc@gmail.com>
2016-05-11 14:43:19 +0000

    Desafio 1014

---
+++ Java 7/1014.java
@@ -0,0 +1,25 @@
+//Nome: Consumo
+//Resultado: Accepted
+//Data: 11/05/16 14:43:19
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        double y;
+        x=sc.nextInt();
+        y = sc.nextDouble();
+        System.out.printf("%.3f km/l\n", (x/y));
+    }
+ 
+}
