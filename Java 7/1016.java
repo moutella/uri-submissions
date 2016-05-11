97e241ee4877000eb5a2b11c29527098a3ff797d
Moutella <joaogabrielmsc@gmail.com>
2016-05-11 14:57:42 +0000

    Desafio 1016

---
+++ Java 7/1016.java
@@ -0,0 +1,24 @@
+//Nome: Distância
+//Resultado: Accepted
+//Data: 11/05/16 14:57:42
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        System.out.printf("%d minutos\n", d*2);
+ 
+ 
+    }
+ 
+}
