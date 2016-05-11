6632e1a0927737dfc5d8b85fd5f895e7f140a956
Moutella <joaogabrielmsc@gmail.com>
2016-05-11 15:01:34 +0000

    Desafio 1017

---
+++ Java 7/1017.java
@@ -0,0 +1,25 @@
+//Nome: Gasto de Combustível
+//Resultado: Accepted
+//Data: 11/05/16 15:01:34
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int t = sc.nextInt();
+        double x =((double)d*(double)t)/12;
+        System.out.printf("%.3f\n", x);
+ 
+    }
+ 
+}
