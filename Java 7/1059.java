335f145a8298c7b000bc0bc47856cd4fdf387a18
Moutella <joaogabrielmsc@gmail.com>
2016-05-11 15:11:28 +0000

    Desafio 1059

---
+++ Java 7/1059.java
@@ -0,0 +1,22 @@
+//Nome: Números Pares
+//Resultado: Accepted
+//Data: 11/05/16 15:11:28
+//Linguagem: Java 7
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        for(int i=1;i<101;i++){
+            if(i%2==0)
+                System.out.println(i);
+        }
+    }
+ 
+}
