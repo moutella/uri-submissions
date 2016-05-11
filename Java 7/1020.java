e172b1ae1838cc7485690c09190fb18f40c6f45a
Moutella <joaogabrielmsc@gmail.com>
2016-05-11 15:04:26 +0000

    Desafio 1020

---
+++ Java 7/1020.java
@@ -0,0 +1,27 @@
+//Nome: Idade em Dias
+//Resultado: Accepted
+//Data: 11/05/16 15:04:26
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a,m,d;
+        d = sc.nextInt();
+        a = d/365;
+        d = d%365;
+        m = d/30;
+        d = d%30;
+        System.out.printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", a, m,d);
+    }
+ 
+}
