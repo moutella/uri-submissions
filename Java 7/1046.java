7c6c2d13d534ae6978989484ca51706ef39bdf66
Moutella <joaogabrielmsc@gmail.com>
2016-05-11 15:08:09 +0000

    Desafio 1046

---
+++ Java 7/1046.java
@@ -0,0 +1,28 @@
+//Nome: Tempo de Jogo
+//Resultado: Wrong answer (40%)
+//Data: 11/05/16 15:08:09
+//Linguagem: Java 7
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int i,f,t;
+        i = sc.nextInt();
+        f = sc.nextInt();
+        t = f-i;
+        if(t<0)
+            t+=24;
+        System.out.printf("O JOGO DUROU %d HORA(S)\n", t);
+ 
+    }
+ 
+}
