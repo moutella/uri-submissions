3f1d1498b9c9e61475a63911e9feb606bac476ba
Moutella <joaogabrielmsc@gmail.com>
2017-12-21 15:10:06 +0000

    Desafio 1871

---
+++ Python 3/1871.py
@@ -0,0 +1,23 @@
+#Nome: Zero vale Zero
+#Resultado: Accepted
+#Data: 21/12/17 15:10:06
+#Linguagem: Python 3
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+import sys
+while 1:
+    n, x = input().split()
+    if n == x == "0":
+        sys.exit()
+    res = int(n) + int(x)
+    strings = str(res).split("0", -1)
+    f = ""
+    for i in range(0, strings.__len__()):
+        f += strings[i]
+    print(f)
