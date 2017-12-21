6111f822810f518c232940b69b32697f08d261f7
Moutella <joaogabrielmsc@gmail.com>
2017-12-21 15:15:26 +0000

    Desafio 2416

---
+++ Python 3/2416.py
@@ -0,0 +1,14 @@
+#Nome: Corrida
+#Resultado: Accepted
+#Data: 21/12/17 15:15:26
+#Linguagem: Python 3
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+x, y = input().split()
+print(int(x) % int(y))
