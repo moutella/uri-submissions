cd220c520a4c101b186f1eb0e6d3116865520227
Moutella <joaogabrielmsc@gmail.com>
2018-07-22 02:39:34 +0000

    Desafio 1030

--- Python 3/1030.py
+++ Python 3/1030.py
@@ -1,23 +1,34 @@
 #Nome: A Lenda de Flavious Josephus
 #Resultado: Time limit exceeded
-#Data: 22/07/18 02:13:36
+#Data: 22/07/18 02:39:34
 #Linguagem: Python 3
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
 nCasos = input()
 for x in range(0, int(nCasos)):
 	casoAtual = input().split()
 	nPessoas = int(casoAtual[0])
 	nPulo = int(casoAtual[1])
-	pessoas = [1] * nPessoas
-	indiceAtual = 0
+	pessoas = []
+	pessoasBack = []
+	for i in range(0, nPessoas):
+		pessoas.append(i+1)
+		pessoasBack.append(i+1)
 	indiceExiste = -1
-	contaExist = 0
-	while pessoas.count(1)>1:
-		if(pessoas[indiceAtual]==1):
-			contaExist += 1
-		if(contaExist == nPulo):
-			pessoas[indiceAtual]=0
-			contaExist = 0
-		indiceAtual+=1
-		if(indiceAtual >= nPessoas):
-			indiceAtual = 0
-	print(pessoas.index(1)+1)
+	atual = 0
+	contador = 0
+	while len(pessoas)>1:
+		contador+=1
+		if (atual >= len(pessoas)):
+			atual = 0
+		if(contador == nPulo):
+			del pessoas[atual]
+			contador = 0
+		else:
+			atual+=1
+	print("Case " +  str(x) + ":", pessoasBack.index(pessoas[0])+1)
