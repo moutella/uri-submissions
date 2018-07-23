5525f660bc11664e973fde3a371b4920f0fcf38f
Moutella <joaogabrielmsc@gmail.com>
2018-07-23 17:14:49 +0000

    Desafio 1011

---
+++ Python 3/1011.py
@@ -0,0 +1,15 @@
+#Nome: Esfera
+#Resultado: Runtime error
+#Data: 23/07/18 17:14:49
+#Linguagem: Python 3
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+raio = float(input())
+
+print("VOLUME = {:.3f}".format(4/3*3.14159*raio*raio*raio)
