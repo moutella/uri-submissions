43e70c6ceb14bdbef4f94b55d1c4f8b16df80138
Moutella <joaogabrielmsc@gmail.com>
2018-07-23 17:11:08 +0000

    Desafio 1009

---
+++ Python 3/1009.py
@@ -0,0 +1,19 @@
+#Nome: Salário com Bônus
+#Resultado: Accepted
+#Data: 23/07/18 17:11:08
+#Linguagem: Python 3
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+nome = input()
+sal = input()
+vendas = input()
+
+total = float(sal) + float(vendas)*0.15
+
+print("TOTAL = R$ {:.2f}".format(total))
