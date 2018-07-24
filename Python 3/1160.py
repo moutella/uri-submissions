6f8b6f2024eac427d7faf9c9c95403cf8c2a966b
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 22:45:51 +0000

    Desafio 1160

---
+++ Python 3/1160.py
@@ -0,0 +1,23 @@
+#Nome: Crescimento Populacional
+#Resultado: Wrong answer (100%)
+#Data: 24/07/18 22:45:51
+#Linguagem: Python 3
+nTestes = int(input())
+
+for i in range(0, nTestes):
+    valores = input().split()
+    APop = float(valores[0])
+    ACresc = float(valores[2])
+    BPop = float(valores[1])
+    BCresc = float(valores[3])
+    anos = 0
+    while BPop > APop:
+        APop += int(ACresc * 0.01 *APop)
+        BPop += int(BCresc * 0.01 * BPop)
+        anos+=1
+        if anos > 100:
+            BPop = 0
+    if anos > 100:
+        print("Mais de 1 seculo.")
+    else:
+        print(anos, "anos.")
