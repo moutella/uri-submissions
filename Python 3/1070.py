31a64b4434a33f1a27d7af644ad2bf3fc67c8068
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 18:53:10 +0000

    Desafio 1070

---
+++ Python 3/1070.py
@@ -0,0 +1,9 @@
+#Nome: Seis Números Ímpares
+#Resultado: Accepted
+#Data: 24/07/18 18:53:10
+#Linguagem: Python 3
+num = int(input())
+if not num%2:
+    num+=1
+for i in range(0,6):
+    print(num + i*2)
