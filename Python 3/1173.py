fed4a0594760b5c55ff9e7bb33353ce1ec907de0
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 22:58:19 +0000

    Desafio 1173

---
+++ Python 3/1173.py
@@ -0,0 +1,8 @@
+#Nome: Preenchimento de Vetor I
+#Resultado: Wrong answer (10%)
+#Data: 24/07/18 22:58:19
+#Linguagem: Python 3
+valor = int(input())
+for i in range(0, 11):
+    print("N[{}] = {}".format(i, valor))
+    valor *= 2
