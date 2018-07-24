d798272b48a436d8c9c6aa3903f888c1d5631b93
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 17:51:45 +0000

    Desafio 1052

--- Python 3/1052.py
+++ Python 3/1052.py
@@ -1,7 +1,32 @@
 #Nome: Mês
-#Resultado: Wrong answer (100%)
-#Data: 24/07/18 17:50:26
+#Resultado: Accepted
+#Data: 24/07/18 17:51:45
 #Linguagem: Python 3
-mes = int(input())
-meses = ["January","February", "March", "April", "June", "July", "August", "September", "October", "November", "December"]
-print(meses[mes-1], end="")
+mes=int(input())
+
+if mes > 0 and  mes <= 12:
+    if mes == 1:
+        print('January')
+    elif mes == 2:
+        print('February')
+    elif mes == 3:
+        print('March')
+    elif mes == 4:
+        print('April')
+    elif mes == 5:
+        print('May')
+    elif mes == 6:
+        print('June')
+    elif mes == 7:
+        print('July')
+    elif mes == 8:
+        print('August')
+    elif mes == 9:
+        print('September')
+    elif mes == 10:
+        print('October')
+    elif mes == 11:
+        print('November')
+    else:
+        print('December')
+
