3535a2d93c3129eec16fd430467296ed2f6c305f
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:44:48 +0000

    Desafio 1144

---
+++ Python 3/1144.py
@@ -0,0 +1,8 @@
+#Nome: Sequência Lógica
+#Resultado: Accepted
+#Data: 24/07/18 20:44:48
+#Linguagem: Python 3
+num = int(input())
+for i in range(1, num+1):
+    print("{} {} {}".format(i, i*i, i*i*i))
+    print("{} {} {}".format(i, i * i + 1, i * i * i + 1))
