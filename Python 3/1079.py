e7ff38ae1a6275847556e9d4b4ad78ae3a82c44a
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 19:03:30 +0000

    Desafio 1079

---
+++ Python 3/1079.py
@@ -0,0 +1,12 @@
+#Nome: Médias Ponderadas
+#Resultado: Accepted
+#Data: 24/07/18 19:03:30
+#Linguagem: Python 3
+num = int(input())
+for i in range(0, num):
+    entradas = input()
+    entrada = entradas.split()
+    A = float(entrada[0])
+    B = float(entrada[1])
+    C = float(entrada[2])
+    print("{:.1f}".format((A*2+B*3+C*5)/10))
