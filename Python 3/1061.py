5560520db2d35fdd0bf1bda62f9f8e2292d8eecc
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 18:45:28 +0000

    Desafio 1061

---
+++ Python 3/1061.py
@@ -0,0 +1,26 @@
+#Nome: Tempo de um Evento
+#Resultado: Accepted
+#Data: 24/07/18 18:45:28
+#Linguagem: Python 3
+diaInicio = int(input().split()[1])
+horaInicio = input().split()
+diaFim = int(input().split()[1])
+horaFim = input().split()
+dias = diaFim - diaInicio
+horaDif = int(horaFim[0])-int(horaInicio[0])
+minFim = int(horaFim[2])-int(horaInicio[2])
+segFim = int(horaFim[4])-int(horaInicio[4])
+
+if segFim < 0:
+    minFim -= 1
+    segFim += 60
+if minFim < 0:
+    horaDif -= 1
+    minFim+=60
+if horaDif < 0:
+    horaDif+=24
+    dias -= 1
+print("{} dia(s)".format(dias))
+print("{} hora(s)".format(horaDif))
+print("{} minuto(s)".format(minFim))
+print("{} segundo(s)".format(segFim))
