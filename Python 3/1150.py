408ec51cfd584b2318051524dcb54e3a97b95b53
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:46:26 +0000

    Desafio 1150

---
+++ Python 3/1150.py
@@ -0,0 +1,16 @@
+#Nome: Ultrapassando Z
+#Resultado: Accepted
+#Data: 24/07/18 21:46:26
+#Linguagem: Python 3
+nInicial = int(input())
+secNum = int(input())
+while secNum <= nInicial:
+    secNum = int(input())
+contador = 0
+
+while secNum > 0:
+    secNum-= nInicial
+    contador+= 1
+    nInicial+=1
+
+print(contador)
