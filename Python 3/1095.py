57f2ed3facf93d3eb809efdf4d44a5d9202f598e
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 19:06:33 +0000

    Desafio 1095

---
+++ Python 3/1095.py
@@ -0,0 +1,8 @@
+#Nome: Sequencia IJ 1
+#Resultado: Wrong answer (10%)
+#Data: 24/07/18 19:06:33
+#Linguagem: Python 3
+jInicial = 60
+for i in range(0,12):
+    print("I={} J={}".format((1+3*i),(jInicial - i * 5)))
+print("I=? J=0")
