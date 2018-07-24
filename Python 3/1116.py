5e6279de72d5562c49a8780e14601bad362845bf
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:08:27 +0000

    Desafio 1116

---
+++ Python 3/1116.py
@@ -0,0 +1,14 @@
+#Nome: Dividindo X por Y
+#Resultado: Accepted
+#Data: 24/07/18 20:08:27
+#Linguagem: Python 3
+nDiv = int(input())
+
+for i in range(0, nDiv):
+    valoresSTR = input().split()
+    A  = int(valoresSTR[0])
+    B = int(valoresSTR[1])
+    if(B == 0):
+        print("divisao impossivel")
+    else:
+        print("{:.1f}".format(A/B))
