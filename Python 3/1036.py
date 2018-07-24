4b3e5760933fa56384a07d6312afe334e646eb05
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 17:11:38 +0000

    Desafio 1036

---
+++ Python 3/1036.py
@@ -0,0 +1,16 @@
+#Nome: Fórmula de Bhaskara
+#Resultado: Accepted
+#Data: 24/07/18 17:11:38
+#Linguagem: Python 3
+import math
+entradas = input()
+entrada = entradas.split()
+A = float(entrada[0])
+B = float(entrada[1])
+C = float(entrada[2])
+raiz = B*B-4*A*C
+if(raiz < 0 or A==0):
+    print("Impossivel calcular")
+else:
+    print("R1 = {:.5f}".format((-B + math.sqrt(raiz))/(2*A)))
+    print("R2 = {:.5f}".format((-B - math.sqrt(raiz) )/ (2 * A)))
