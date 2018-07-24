34e861afd3149912915b4946f68de5fa4e35800b
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:40:29 +0000

    Desafio 1154

--- Python 3/1154.py
+++ Python 3/1154.py
@@ -1,13 +1,13 @@
 #Nome: Idades
-#Resultado: Wrong answer (100%)
-#Data: 24/07/18 21:39:44
+#Resultado: Accepted
+#Data: 24/07/18 21:40:29
 #Linguagem: Python 3
 soma = 0
 contador = 0
 while 1:
     valor = int(input())
     if(valor<0):
-        print(soma/contador)
+        print("{:.2f}".format(soma/contador))
         break
     soma += valor
     contador+=1
