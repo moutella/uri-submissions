e22b12ec5c5f22176b2f99cfc6b4bd6840b8b636
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 18:57:53 +0000

    Desafio 1073

--- Python 3/1073.py
+++ Python 3/1073.py
@@ -1,9 +1,9 @@
 #Nome: Quadrado de Pares
-#Resultado: Wrong answer (100%)
-#Data: 24/07/18 18:57:13
+#Resultado: Accepted
+#Data: 24/07/18 18:57:53
 #Linguagem: Python 3
 num = int(input())
-num = 7 // 2
+num = num // 2
 for i in range(0, num):
     n = 2 + i*2
     print("{}^2 = {}".format(n, n*n))
