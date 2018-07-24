6c1acd9549a0c78f662b97dd49e90427b1eb857b
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:31:45 +0000

    Desafio 1131

--- Python 3/1131.py
+++ Python 3/1131.py
@@ -1,12 +1,14 @@
 #Nome: Grenais
-#Resultado: Wrong answer (100%)
-#Data: 24/07/18 20:30:06
+#Resultado: Accepted
+#Data: 24/07/18 20:31:45
 #Linguagem: Python 3
 continua = 1
 interVit = 0
 greVit = 0
 empt = 0
+nGrenais = 0
 while(continua==1):
+    nGrenais +=1
     grenalstr = input().split()
     intGol = int(grenalstr[0])
     greGol = int(grenalstr[1])
@@ -20,6 +22,10 @@ while(continua==1):
     print("Novo grenal (1-sim 2-nao)")
     continua = int(input())
 
+print("{} grenais".format(nGrenais))
+print("Inter:{}".format(interVit))
+print("Gremio:{}".format(greVit))
+print("Empates:{}".format(empt))
 if(interVit>greVit):
     print("Inter venceu mais")
 else:
