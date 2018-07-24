8ea4397eb9bd974ed3ce0c0d52237fc972f3a8c2
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 22:51:30 +0000

    Desafio 1164

---
+++ Python 3/1164.py
@@ -0,0 +1,19 @@
+#Nome: Número Perfeito
+#Resultado: Accepted
+#Data: 24/07/18 22:51:30
+#Linguagem: Python 3
+nTestes = int(input())
+
+for i in range(0, nTestes):
+    valor = int(input())
+    divisores = []
+    soma = 0
+    for i in range(1, int(valor/2)+1):
+        if not valor % i:
+            divisores.append(i)
+    for val in divisores:
+        soma += val
+    if soma == valor:
+        print("{} eh perfeito".format(valor))
+    else:
+        print("{} nao eh perfeito".format(valor))
