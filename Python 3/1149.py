be647babbcc45c9af18e0b163229f253bfc79060
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:27:09 +0000

    Desafio 1149

---
+++ Python 3/1149.py
@@ -0,0 +1,11 @@
+#Nome: Somando Inteiros Consecutivos
+#Resultado: Accepted
+#Data: 24/07/18 21:27:09
+#Linguagem: Python 3
+valores = input().split()
+A = int(valores[0])
+B = int(valores[-1])
+total = 0
+for i in range(0, B):
+    total+=(A+i)
+print(total)
