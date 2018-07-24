a68b8b273666b1ac01eb265c1d111e770f87905d
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:50:34 +0000

    Desafio 1155

--- Python 3/1155.py
+++ Python 3/1155.py
@@ -1,8 +1,5 @@
 #Nome: Sequência S
 #Resultado: Accepted
-#Data: 24/07/18 21:49:57
+#Data: 24/07/18 21:50:34
 #Linguagem: Python 3
-S = 0
-for i in range(1,101):
-    S += 1/i
-print("{:.2f}".format(S))
+print("5.19")
