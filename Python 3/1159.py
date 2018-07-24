91d0c57d0bdce31b07c6567ecb391fba28168d5f
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 22:01:34 +0000

    Desafio 1159

---
+++ Python 3/1159.py
@@ -0,0 +1,14 @@
+#Nome: Soma de Pares Consecutivos
+#Resultado: Accepted
+#Data: 24/07/18 22:01:34
+#Linguagem: Python 3
+while 1:
+    teste = int(input())
+    if(teste == 0):
+        break
+    soma = 0
+    if(teste%2):
+        teste+=1
+    for j in range(0, 5):
+        soma += teste + j*2
+    print(soma)
