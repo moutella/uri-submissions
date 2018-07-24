f13474a099b4cc3267d3d0d09525e7cb313588d0
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:35:45 +0000

    Desafio 1151

---
+++ Python 3/1151.py
@@ -0,0 +1,15 @@
+#Nome: Fibonacci Fácil
+#Resultado: Presentation error
+#Data: 24/07/18 21:35:45
+#Linguagem: Python 3
+nValores = int(input())
+temp = 0
+nAnt = 0
+nAtual = 1
+print("0 1", end =" ")
+nAtual = nAtual + nAnt
+for i in range(0, nValores - 2):
+    temp = nAnt
+    nAnt = nAtual
+    nAtual = nAtual + temp
+    print(nAtual, end = " ")
