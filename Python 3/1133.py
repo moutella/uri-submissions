339d31ff6971fd868330b90f1e822824975d04a9
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:36:47 +0000

    Desafio 1133

---
+++ Python 3/1133.py
@@ -0,0 +1,15 @@
+#Nome: Resto da Divisão
+#Resultado: Wrong answer (10%)
+#Data: 24/07/18 20:36:47
+#Linguagem: Python 3
+A = int(input())
+B = int(input())
+soma = 0
+if(A>B):
+    A ^= B
+    B ^= A
+    A ^= B
+for i in range(A,B):
+    resto = i%5
+    if resto == 2 or resto == 3:
+        print(i)
