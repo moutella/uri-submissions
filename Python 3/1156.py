b5d62e9e5024d3a0f7443864469afe2e43b5a99f
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:53:58 +0000

    Desafio 1156

---
+++ Python 3/1156.py
@@ -0,0 +1,11 @@
+#Nome: Sequência S II
+#Resultado: Accepted
+#Data: 24/07/18 21:53:58
+#Linguagem: Python 3
+S = 0
+frac = 1
+for i in range(1, 39, 2):
+    S += i / frac
+    frac*=2
+
+print("{:.2f}".format(S))
