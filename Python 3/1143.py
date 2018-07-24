fbeb81f7fc4384f6c111c3d26ca8e45def964cf1
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:41:23 +0000

    Desafio 1143

---
+++ Python 3/1143.py
@@ -0,0 +1,7 @@
+#Nome: Quadrado e ao Cubo
+#Resultado: Accepted
+#Data: 24/07/18 20:41:23
+#Linguagem: Python 3
+num = int(input())
+for i in range(1, num+1):
+    print("{} {} {}".format(i, i*i, i*i*i))
