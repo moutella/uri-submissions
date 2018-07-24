6d2967fd9400187257b99acd2e73733463a9766a
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 18:51:41 +0000

    Desafio 1065

---
+++ Python 3/1065.py
@@ -0,0 +1,12 @@
+#Nome: Pares entre Cinco Números
+#Resultado: Accepted
+#Data: 24/07/18 18:51:41
+#Linguagem: Python 3
+valores = []
+nPar = 0
+for i in range(0, 5):
+    x = int(input())
+    if not (x % 2):
+        nPar+=1
+        valores.append(x)
+print("{} valores pares".format(nPar))
