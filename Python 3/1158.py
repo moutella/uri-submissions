8bcf2a082dd2466a3258b9f1bcccf9eeb9b33189
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:58:36 +0000

    Desafio 1158

---
+++ Python 3/1158.py
@@ -0,0 +1,15 @@
+#Nome: Soma de Ímpares Consecutivos III
+#Resultado: Accepted
+#Data: 24/07/18 21:58:36
+#Linguagem: Python 3
+nTestes = int(input())
+for i in range(0,nTestes):
+    teste = input().split()
+    A = int(teste[0])
+    B = int(teste[1])
+    soma = 0
+    if(not A%2):
+        A+=1
+    for j in range(0, B):
+        soma += A + j*2
+    print(soma)
