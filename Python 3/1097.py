0acac5eba0b2cbbbe4cd0e33f43111f45de12b4a
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 19:12:55 +0000

    Desafio 1097

---
+++ Python 3/1097.py
@@ -0,0 +1,7 @@
+#Nome: Sequencia IJ 3
+#Resultado: Accepted
+#Data: 24/07/18 19:12:55
+#Linguagem: Python 3
+for i in range(1,10,2):
+    for j in range(7,4,-1):
+        print("I={} J={}".format(i,j+i-1))
