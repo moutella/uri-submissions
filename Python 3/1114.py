d8d8dc549a36397cd210b82b211ceb63078d10b7
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:04:52 +0000

    Desafio 1114

---
+++ Python 3/1114.py
@@ -0,0 +1,11 @@
+#Nome: Senha Fixa
+#Resultado: Presentation error
+#Data: 24/07/18 20:04:52
+#Linguagem: Python 3
+while 1:
+    senha = input()
+    if(senha=="2002"):
+        print("Acesso permitido")
+        break
+    else:
+        print("Senha Invalida")
