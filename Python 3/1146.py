a9b70e53f294b2a0cb48143950e0370ed30560f1
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:01:43 +0000

    Desafio 1146

---
+++ Python 3/1146.py
@@ -0,0 +1,10 @@
+#Nome: Sequências Crescentes
+#Resultado: Accepted
+#Data: 24/07/18 21:01:43
+#Linguagem: Python 3
+seqFim = int(input())
+while seqFim:
+    for i in range(1, seqFim):
+        print(i, end=" ")
+    print(seqFim)
+    seqFim = int(input())
