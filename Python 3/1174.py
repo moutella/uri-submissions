8102e507f5fa7b39e753b31c4217215e0e6cd514
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 23:07:45 +0000

    Desafio 1174

--- Python 3/1174.py
+++ Python 3/1174.py
@@ -1,8 +1,8 @@
 #Nome: Seleçao em Vetor I
-#Resultado: Wrong answer (100%)
-#Data: 24/07/18 23:07:24
+#Resultado: Accepted
+#Data: 24/07/18 23:07:45
 #Linguagem: Python 3
 for i in range(0,100):
     valor = float(input())
     if valor <= 10:
-        print("A[{}] = {:.2f}".format(i, valor))
+        print("A[{}] = {:.1f}".format(i, valor))
