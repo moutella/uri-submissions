ac11831c609d99b3891defbb80b0b39500047590
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:49:32 +0000

    Desafio 1145

---
+++ Python 3/1145.py
@@ -0,0 +1,13 @@
+#Nome: Sequência Lógica 2
+#Resultado: Presentation error
+#Data: 24/07/18 20:49:32
+#Linguagem: Python 3
+entradas = input()
+entrada = entradas.split()
+A = int(entrada[0])
+B = int(entrada[1])
+
+for i in range(1, B+1):
+    print(i,"", end = "")
+    if not i % A:
+        print()
