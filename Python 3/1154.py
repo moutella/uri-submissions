ac25bab3b7ff24a9a97bb9afa18aaa2449dcb1f5
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 21:39:44 +0000

    Desafio 1154

---
+++ Python 3/1154.py
@@ -0,0 +1,13 @@
+#Nome: Idades
+#Resultado: Wrong answer (100%)
+#Data: 24/07/18 21:39:44
+#Linguagem: Python 3
+soma = 0
+contador = 0
+while 1:
+    valor = int(input())
+    if(valor<0):
+        print(soma/contador)
+        break
+    soma += valor
+    contador+=1
