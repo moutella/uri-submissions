ee105a3ebcdc1cb7d276165be4b2f12af1389ed9
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 19:18:10 +0000

    Desafio 1098

--- Python 3/1098.py
+++ Python 3/1098.py
@@ -1,10 +1,10 @@
 #Nome: Sequencia IJ 4
-#Resultado: Wrong answer (10%)
-#Data: 24/07/18 19:17:20
+#Resultado: Accepted
+#Data: 24/07/18 19:18:10
 #Linguagem: Python 3
 for i in range(0,11):
     for j in range(1,4):
         if(float(i*0.2)%1):
-            print("I={:.1f} J={}".format(i*0.2,j + i * 0.2))
+            print("I={:.1f} J={:.1f}".format(i*0.2,j + i * 0.2))
         else:
             print("I={} J={}".format(int(i * 0.2), j + int(i*0.2)))
