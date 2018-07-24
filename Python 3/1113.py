39a7b89403939b0ad350c51a699c48d0e8087deb
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:03:16 +0000

    Desafio 1113

---
+++ Python 3/1113.py
@@ -0,0 +1,14 @@
+#Nome: Crescente e Decrescente
+#Resultado: Accepted
+#Data: 24/07/18 20:03:16
+#Linguagem: Python 3
+while 1:
+    valores = input().split()
+    A = int(valores[0])
+    B = int(valores[1])
+    if(A==B):
+        break
+    if(A>B):
+        print("Decrescente")
+    else:
+        print("Crescente")
