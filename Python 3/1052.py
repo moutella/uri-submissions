eccf47855c5e63a200928fdd620065414ec5a9ae
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 17:46:37 +0000

    Desafio 1052

---
+++ Python 3/1052.py
@@ -0,0 +1,7 @@
+#Nome: Mês
+#Resultado: Wrong answer (100%)
+#Data: 24/07/18 17:46:37
+#Linguagem: Python 3
+meses = ["January","February", "March", "April", "June", "July", "August", "September", "October", "November", "December"]
+mes = int(input())
+print(meses[mes-1])
