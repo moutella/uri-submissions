833636cd0b5fd39656779b529eb7d7b074e89afe
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 23:28:52 +0000

    Desafio 1176

---
+++ Python 3/1176.py
@@ -0,0 +1,22 @@
+#Nome: Fibonacci em Vetor
+#Resultado: Runtime error
+#Data: 24/07/18 23:28:52
+#Linguagem: Python 3
+nValores = int(input())
+temp = 0
+nAnt = 0
+nAtual = 1
+
+nAtual = nAtual + nAnt
+
+fib = []
+fib.append(0)
+fib.append(1)
+for i in range(3, 60):
+    temp = nAnt
+    nAnt = nAtual
+    nAtual = nAtual + temp
+    fib.append(nAtual)
+for j in range(0, nValores):
+    indice = int(input())
+    print("Fib({}) = {}".format(indice, fib[indice]))
