6b77e3b596a60a9a91447d06e11216bdc79d8eee
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 20:33:22 +0000

    Desafio 1132

---
+++ Python 3/1132.py
@@ -0,0 +1,11 @@
+#Nome: Múltiplos de 13
+#Resultado: Wrong answer (100%)
+#Data: 24/07/18 20:33:22
+#Linguagem: Python 3
+A = int(input())
+B = int(input())
+soma = 0
+for i in range(A,B+1):
+    if i % 13:
+        soma+=i
+print(soma)
