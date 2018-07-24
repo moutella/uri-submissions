d74ae652d6492a8373fe9a2997fdc0eede020a21
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 23:32:29 +0000

    Desafio 1177

---
+++ Python 3/1177.py
@@ -0,0 +1,7 @@
+#Nome: Preenchimento de Vetor II
+#Resultado: Accepted
+#Data: 24/07/18 23:32:29
+#Linguagem: Python 3
+valor = int(input())
+for i in range(0,1000):
+    print("N[{}] = {}".format(i, i%valor))
