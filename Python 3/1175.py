71f92b5002e9ffb114af14f33296b4539b792440
Moutella <joaogabrielmsc@gmail.com>
2018-07-24 23:10:50 +0000

    Desafio 1175

---
+++ Python 3/1175.py
@@ -0,0 +1,10 @@
+#Nome: Troca em Vetor I
+#Resultado: Wrong answer (100%)
+#Data: 24/07/18 23:10:50
+#Linguagem: Python 3
+vetor = []
+for i in range(19,-1,-1):
+    valor = int(input())
+    vetor.append(valor)
+for i in range(0,20):
+    print("N[{}] = {}".format(i, vetor[i]))
