382ef56767ccff6e648d8eafdc859019849db22b
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 20:40:32 +0000

    Desafio 1541

---
+++ Python 3/1541.py
@@ -0,0 +1,13 @@
+#Nome: Construindo Casas
+#Resultado: Accepted
+#Data: 25/07/18 20:40:32
+#Linguagem: Python 3
+import math
+while 1:
+    entrada = input()
+    if entrada == "0":
+        break
+    valores = entrada.split()
+    tamanho = int(valores[0])*int(valores[1])
+    proporcao = 100/int(valores[2])
+    print(int(math.sqrt(tamanho*proporcao)))
