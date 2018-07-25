02daf5ee08a7e0bc8c8529739381a802a4591890
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:44:43 +0000

    Desafio 1187

---
+++ Python 3/1187.py
@@ -0,0 +1,20 @@
+#Nome: Área Superior
+#Resultado: Wrong answer (50%)
+#Data: 25/07/18 13:44:43
+#Linguagem: Python 3
+op = input()
+matriz = []
+for i in range(0,12):
+    matriz.append([])
+    for j in range(0, 12):
+        matriz[i].append(float(input()))
+
+soma = 0
+for k in range(0, 6):
+    for j in range(k+1,11-k):
+        soma+= matriz[k][j]
+
+if(op=="S"):
+    print("{:.1f}".format(soma))
+else:
+    print("{:.1f}".format(soma/66))
