4ed4ab51f7dd8fb57c3d767effc179197092ef3a
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 20:56:13 +0000

    Desafio 1557

---
+++ Python 3/1557.py
@@ -0,0 +1,25 @@
+#Nome: Matriz Quadrada III
+#Resultado: Runtime error
+#Data: 25/07/18 20:56:13
+#Linguagem: Python 3
+import math
+while 1:
+    entrada = int(input())
+    if entrada == "0":
+        break
+    caracteres = 1
+    x = 2 ** (entrada-1)
+    car = entrada
+    while x >= 10:
+        x/=10
+        caracteres+=1
+    print(caracteres)
+    for i in range(0, entrada):
+        for j in range(0, entrada):
+            valor = min(2**j, 2**i)
+
+            print("{0:>{x}}".format(valor, x=caracteres), end="")
+            if(j!=entrada-1):
+                print(" ", end="")
+        print()
+    print()
