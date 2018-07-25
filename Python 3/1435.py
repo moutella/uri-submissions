c38476f39f54345098ca2fb502ae7207753df67d
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 16:09:06 +0000

    Desafio 1435

--- Python 3/1435.py
+++ Python 3/1435.py
@@ -1,10 +1,11 @@
 #Nome: Matriz Quadrada I
 #Resultado: Presentation error
-#Data: 25/07/18 16:08:44
+#Data: 25/07/18 16:09:06
 #Linguagem: Python 3
 while 1:
     tamanho = int(input())
     if tamanho == 0:
+        print()
         break
     tamanho+=1
     o=tamanho
