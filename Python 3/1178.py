cb809f0e3ac0ff8964e1d37c316bcd518601c615
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:09:40 +0000

    Desafio 1178

---
+++ Python 3/1178.py
@@ -0,0 +1,8 @@
+#Nome: Preenchimento de Vetor III
+#Resultado: Accepted
+#Data: 25/07/18 13:09:40
+#Linguagem: Python 3
+valor = float(input())
+for i in range(0,100):
+    print("N[{}] = {:.4f}".format(i, valor))
+    valor/=2
