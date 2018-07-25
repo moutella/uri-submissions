8091b32a5092bc0ff11a2244743f8aafeb1ff560
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:40:16 +0000

    Desafio 1185

---
+++ Python 3/1185.py
@@ -0,0 +1,20 @@
+#Nome: Acima da Diagonal Secundária
+#Resultado: Accepted
+#Data: 25/07/18 13:40:16
+#Linguagem: Python 3
+op = input()
+matriz = []
+for i in range(0,12):
+    matriz.append([])
+    for j in range(0, 12):
+        matriz[i].append(float(input()))
+
+soma = 0
+for k in range(11, -1,-1):
+    for j in range(0,k):
+        soma+= matriz[11-k][j]
+
+if(op=="S"):
+    print("{:.1f}".format(soma))
+else:
+    print("{:.1f}".format(soma/66))
