61b87508320ee48dd02813c5f0a9d75bcac83a36
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 22:25:16 +0000

    Desafio 1864

---
+++ Python 3/1864.py
@@ -0,0 +1,7 @@
+#Nome: Nossos Dias Nunca Voltarão
+#Resultado: Wrong answer (10%)
+#Data: 25/07/18 22:25:16
+#Linguagem: Python 3
+frase = "LIFE IS NOT A PROBLEM TO BE SOLVE"
+fim = int(input())
+print(frase[0:fim])
