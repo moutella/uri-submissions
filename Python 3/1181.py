f70b35d32df4656945426e4fc7886906b9f84987
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:32:01 +0000

    Desafio 1181

---
+++ Python 3/1181.py
@@ -0,0 +1,20 @@
+#Nome: Linha na Matriz
+#Resultado: Accepted
+#Data: 25/07/18 13:32:01
+#Linguagem: Python 3
+linha = int(input())
+op = input()
+matriz = []
+for i in range(0,12):
+    matriz.append([])
+    for j in range(0, 12):
+        matriz[i].append(float(input()))
+
+soma = 0
+for k in range(0, 12):
+    soma += matriz[linha][k]
+
+if(op=="S"):
+    print("{:.1f}".format(soma))
+else:
+    print("{:.1f}".format(soma/12))
