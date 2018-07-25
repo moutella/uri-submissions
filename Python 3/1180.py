f76762fe9f65c118285a361d6824fa2787c7fa06
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:17:53 +0000

    Desafio 1180

---
+++ Python 3/1180.py
@@ -0,0 +1,14 @@
+#Nome: Menor e Posição
+#Resultado: Presentation error
+#Data: 25/07/18 13:17:53
+#Linguagem: Python 3
+valor = int(input())
+valores = input().split()
+menor = int(valores[0])
+pos = 0
+for valor in valores:
+    if int(valor)<menor:
+        menor = int(valor)
+        pos = valores.index(valor)
+print("Menor valor:", menor)
+print("Posicao: ", pos)
