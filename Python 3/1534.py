19cea22234e3c4b68dc0b743074acc343f4fb58b
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 20:28:32 +0000

    Desafio 1534

---
+++ Python 3/1534.py
@@ -0,0 +1,15 @@
+#Nome: Matriz 123
+#Resultado: Runtime error
+#Data: 25/07/18 20:28:32
+#Linguagem: Python 3
+while 1:
+    tamanho = int(input())
+    for i in range(0, tamanho):
+        for j in range(0, tamanho):
+            if i==j:
+                print("1", end="")
+            elif i == 3-j:
+                print("2", end="")
+            else:
+                print("3", end="")
+        print()
