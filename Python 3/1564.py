68c193c1ad6b9b6ca14f294bd9b26ddf833875f0
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 22:11:27 +0000

    Desafio 1564

---
+++ Python 3/1564.py
@@ -0,0 +1,13 @@
+#Nome: Vai Ter Copa?
+#Resultado: Accepted
+#Data: 25/07/18 22:11:27
+#Linguagem: Python 3
+while 1:
+    try:
+        entrada = int(input())
+        if entrada > 0:
+            print("vai ter duas!")
+        else:
+            print("vai ter copa!")
+    except EOFError:
+        break
