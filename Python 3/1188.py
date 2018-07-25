9e23fbfab3c8e4d27dab79d9ff77e6aec39c1644
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:46:17 +0000

    Desafio 1188

--- Python 3/1188.py
+++ Python 3/1188.py
@@ -1,6 +1,6 @@
 #Nome: Área Inferior
 #Resultado: Wrong answer (100%)
-#Data: 25/07/18 13:45:39
+#Data: 25/07/18 13:46:17
 #Linguagem: Python 3
 op = input()
 matriz = []
@@ -10,9 +10,9 @@ for i in range(0,12):
         matriz[i].append(float(input()))
 
 soma = 0
-for k in range(6, 12):
+for k in range(0, 6):
     for j in range(k+1,11-k):
-        soma+= matriz[k][j]
+        soma+= matriz[k+6][j]
 
 if(op=="S"):
     print("{:.1f}".format(soma))
