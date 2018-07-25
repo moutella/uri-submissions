e84a5cdd4c30745b9393890ea1a9e95da94b7905
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 22:15:44 +0000

    Desafio 1759

---
+++ Python 3/1759.py
@@ -0,0 +1,10 @@
+#Nome: Ho Ho Ho
+#Resultado: Accepted
+#Data: 25/07/18 22:15:44
+#Linguagem: Python 3
+nVal = int(input())
+for i in range(0, nVal):
+    print("Ho", end="")
+    if(i != nVal-1):
+        print(" ", end="")
+print("!")
