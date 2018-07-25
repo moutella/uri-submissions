5744295a79e56cd48e086fcb4b78b0e8a8edac1f
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 15:24:32 +0000

    Desafio 1435

---
+++ Python 3/1435.py
@@ -0,0 +1,16 @@
+#Nome: Matriz Quadrada I
+#Resultado: Presentation error
+#Data: 25/07/18 15:24:32
+#Linguagem: Python 3
+while 1:
+    tamanho = int(input())
+    if tamanho == 0:
+        break
+    tamanho+=1
+    o=tamanho
+    for i in range(1, tamanho):
+        for j in range(1, tamanho):
+            print(min(i,j,abs(j-o), abs(i-o)), end=" ")
+        print()
+    print()
+print()
