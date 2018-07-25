237f1d5df0aebb9d1ce9a628a90e7ea03b2afd13
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 22:14:36 +0000

    Desafio 1589

---
+++ Python 3/1589.py
@@ -0,0 +1,8 @@
+#Nome: Bob Conduite
+#Resultado: Accepted
+#Data: 25/07/18 22:14:36
+#Linguagem: Python 3
+nVal = int(input())
+for i in range(0, nVal):
+    val = input().split()
+    print(int(val[0])+int(val[1]))
