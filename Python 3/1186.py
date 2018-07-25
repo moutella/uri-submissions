d8c852548afd959fbaacdee378000aa5118fed6b
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:40:47 +0000

    Desafio 1186

--- Python 3/1186.py
+++ Python 3/1186.py
@@ -1,6 +1,6 @@
 #Nome: Abaixo da Diagonal Secundária
-#Resultado: Wrong answer (100%)
-#Data: 25/07/18 13:40:35
+#Resultado: Accepted
+#Data: 25/07/18 13:40:47
 #Linguagem: Python 3
 op = input()
 matriz = []
@@ -12,7 +12,7 @@ for i in range(0,12):
 soma = 0
 for k in range(11, -1,-1):
     for j in range(0,k):
-        soma+= matriz[j][11-k]
+        soma+= matriz[11-j][k]
 
 if(op=="S"):
     print("{:.1f}".format(soma))
