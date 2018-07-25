31d0305f7064254de4fa7f872826c7e991cf73eb
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 21:51:40 +0000

    Desafio 1557

--- Python 3/1557.py
+++ Python 3/1557.py
@@ -1,23 +1,26 @@
 #Nome: Matriz Quadrada III
-#Resultado: Runtime error
-#Data: 25/07/18 21:50:38
+#Resultado: Presentation error
+#Data: 25/07/18 21:51:40
 #Linguagem: Python 3
 import math
-while 1:
-    entrada = int(input())
-    if entrada == "0":
-        break
-    caracteres = 1
-    x = 2 ** (entrada*2)
-    car = entrada
-    while x >= 10:
-        x/=10
-        caracteres+=1
-    for i in range(0, entrada):
-        for j in range(0, entrada):
-            valor = 2**(j+i)
-            print("{0:>{x}}".format(valor, x=caracteres), end="")
-            if(j!=entrada-1):
-                print(" ", end="")
+try:
+    while 1:
+        entrada = int(input())
+        if entrada == "0":
+            break
+        caracteres = 1
+        x = 2 ** (entrada*2)
+        car = entrada
+        while x >= 10:
+            x/=10
+            caracteres+=1
+        for i in range(0, entrada):
+            for j in range(0, entrada):
+                valor = 2**(j+i)
+                print("{0:>{x}}".format(valor, x=caracteres), end="")
+                if(j!=entrada-1):
+                    print(" ", end="")
+            print()
         print()
-    print()
+except EOFError:
+    pass
