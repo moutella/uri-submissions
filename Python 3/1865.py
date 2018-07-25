689d452b894706d7d4156c1449b16c01afd89b0a
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 22:23:50 +0000

    Desafio 1865

---
+++ Python 3/1865.py
@@ -0,0 +1,11 @@
+#Nome: Mjölnir
+#Resultado: Accepted
+#Data: 25/07/18 22:23:50
+#Linguagem: Python 3
+nVal = int(input())
+for i in range(nVal):
+    pode = input().split()[0] == "Thor"
+    if pode:
+        print("Y")
+    else:
+        print("N")
