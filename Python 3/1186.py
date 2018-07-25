578dd73609791902016dae0699049e8ac7bcd9e7
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 13:40:35 +0000

    Desafio 1186

---
+++ Python 3/1186.py
@@ -0,0 +1,20 @@
+#Nome: Abaixo da Diagonal Secundária
+#Resultado: Wrong answer (100%)
+#Data: 25/07/18 13:40:35
+#Linguagem: Python 3
+op = input()
+matriz = []
+for i in range(0,12):
+    matriz.append([])
+    for j in range(0, 12):
+        matriz[i].append(float(input()))
+
+soma = 0
+for k in range(11, -1,-1):
+    for j in range(0,k):
+        soma+= matriz[j][11-k]
+
+if(op=="S"):
+    print("{:.1f}".format(soma))
+else:
+    print("{:.1f}".format(soma/66))
