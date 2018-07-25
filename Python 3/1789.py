8363073fdb2dfc83151a71c22e4f7202bf00d369
Moutella <joaogabrielmsc@gmail.com>
2018-07-25 22:19:42 +0000

    Desafio 1789

--- Python 3/1789.py
+++ Python 3/1789.py
@@ -1,17 +1,20 @@
 #Nome: A Corrida de Lesmas
-#Resultado: Runtime error
-#Data: 25/07/18 22:19:04
+#Resultado: Accepted
+#Data: 25/07/18 22:19:42
 #Linguagem: Python 3
-while 1:
-    nVal = int(input())
-    vels = input().split()
-    maior = 0
-    for vel in vels:
-        if(int(vel)>maior):
-            maior = int(vel)
-    if(maior < 10):
-        print("1")
-    elif(maior<20):
-        print("2")
-    else:
-        print("3")
+try:
+    while 1:
+        nVal = int(input())
+        vels = input().split()
+        maior = 0
+        for vel in vels:
+            if(int(vel)>maior):
+                maior = int(vel)
+        if(maior < 10):
+            print("1")
+        elif(maior<20):
+            print("2")
+        else:
+            print("3")
+except EOFError:
+    pass
