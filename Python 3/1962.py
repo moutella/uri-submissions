6d73216ac0908a8d414274ceb4f620f5af135c71
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 21:33:18 +0000

    Desafio 1962

---
+++ Python 3/1962.py
@@ -0,0 +1,13 @@
+#Nome: Há Muito, Muito Tempo Atrás
+#Resultado: Accepted
+#Data: 29/07/18 21:33:18
+#Linguagem: Python 3
+nvals = int(input())
+for i in range(nvals):
+    x = int(input())
+    y = 2015 - x
+    if y <= 0:
+        y -= 1
+        print("{} A.C.".format(abs(y)))
+    else:
+        print("{} D.C.".format(y))
