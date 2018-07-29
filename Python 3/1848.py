925018e0261a53ff0854f8a97ee4afe41984d8ca
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:06:35 +0000

    Desafio 1848

---
+++ Python 3/1848.py
@@ -0,0 +1,15 @@
+#Nome: Corvo Contador
+#Resultado: Wrong answer (50%)
+#Data: 29/07/18 19:06:35
+#Linguagem: Python 3
+count = 0
+soma = 0
+while count < 3:
+    momento = input()
+    if momento == "caw caw":
+        count+=1
+        print(soma)
+        soma = 0
+    else:
+        x = 2 - momento.index("*")
+        soma += 2 ** x
