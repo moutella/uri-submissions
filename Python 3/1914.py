32b4bbec44100a69a09a52bc587e609bc4d55157
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:20:32 +0000

    Desafio 1914

---
+++ Python 3/1914.py
@@ -0,0 +1,12 @@
+#Nome: De Quem é a Vez?
+#Resultado: Accepted
+#Data: 29/07/18 19:20:32
+#Linguagem: Python 3
+nCasos = int(input())
+for i in range(nCasos):
+    parImp = input().split()
+    valores = input().split()
+    if not (int(valores[0]) + int(valores[1]))%2:
+        print(parImp[parImp.index("PAR")-1])
+    else:
+        print(parImp[parImp.index("IMPAR") - 1])
