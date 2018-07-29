e6a7d431c5cf4123469f3c1434a338d494870f0c
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:40:49 +0000

    Desafio 1929

--- Python 3/1929.py
+++ Python 3/1929.py
@@ -1,13 +1,13 @@
 #Nome: Triângulo
-#Resultado: Wrong answer (10%)
-#Data: 29/07/18 19:39:55
+#Resultado: Accepted
+#Data: 29/07/18 19:40:49
 #Linguagem: Python 3
 valores = input().split()
 vals = []
 for n in valores:
     vals.append(int(n))
 vals.sort()
-if(vals[3] > vals[2] + vals[1]) and (vals[2] > vals[1]+vals[0]):
+if(vals[3] >= vals[2] + vals[1]) and (vals[2] >= vals[1]+vals[0]):
     print("N")
 else:
     print("S")
