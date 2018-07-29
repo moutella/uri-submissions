03b0dbfab80bab8c3ac3075f248e3464cc7642fc
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 18:37:34 +0000

    Desafio 1837

---
+++ Python 3/1837.py
@@ -0,0 +1,11 @@
+#Nome: Prefácio
+#Resultado: Accepted
+#Data: 29/07/18 18:37:34
+#Linguagem: Python 3
+valores = input().split()
+b = int(valores[0])%int(valores[1])
+a = int(valores[0])//int(valores[1])
+if(b<0):
+    a+=1
+    b+=abs(int(valores[1]))
+print(a, b)
