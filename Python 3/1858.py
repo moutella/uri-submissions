a6da61a6b092a6815f28f483321735215042effa
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:17:38 +0000

    Desafio 1858

---
+++ Python 3/1858.py
@@ -0,0 +1,10 @@
+#Nome: A Resposta de Theon
+#Resultado: Accepted
+#Data: 29/07/18 19:17:38
+#Linguagem: Python 3
+nVal = int(input())
+valStr = input().split()
+valores =[]
+for str in valStr:
+    valores.append(int(str))
+print(valores.index(min(valores))+1)
