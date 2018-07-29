13664d9f60df172be0d1f3db5197ca0db6afc920
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 21:25:44 +0000

    Desafio 1961

---
+++ Python 3/1961.py
@@ -0,0 +1,12 @@
+#Nome: Pula Sapo
+#Resultado: Accepted
+#Data: 29/07/18 21:25:44
+#Linguagem: Python 3
+valores = input().split()
+canos = input().split()
+for x in range(int(valores[1])-1):
+    if abs(int(canos[x]) - int(canos[x + 1])) > int(valores[0]):
+        print("GAME OVER")
+        exit()
+
+print("YOU WIN")
