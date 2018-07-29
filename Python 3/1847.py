a905c6237abc7775af95988e76eb30f6d40c5238
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:00:08 +0000

    Desafio 1847

--- Python 3/1847.py
+++ Python 3/1847.py
@@ -1,13 +1,15 @@
 #Nome: Bem
-#Resultado: Wrong answer (20%)
-#Data: 29/07/18 18:52:36
+#Resultado: Accepted
+#Data: 29/07/18 19:00:08
 #Linguagem: Python 3
 valores = input().split()
 a = int(valores[0])
 b = int(valores[1])
 c = int(valores[2])
 
-if(b-a <= c-b):
+if(b-a < c-b):
+    print(":)")
+elif(c > b and b-a == c-b):
     print(":)")
 else:
     print(":(")
