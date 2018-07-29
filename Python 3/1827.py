969f6e8a2dfce052d866ecc084a9daeeb6e1e38e
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 16:53:57 +0000

    Desafio 1827

--- Python 3/1827.py
+++ Python 3/1827.py
@@ -1,16 +1,21 @@
 #Nome: Matriz Quadrada IV
-#Resultado: Wrong answer (10%)
-#Data: 29/07/18 16:49:07
+#Resultado: Time limit exceeded
+#Data: 29/07/18 16:53:57
 #Linguagem: Python 3
 while 1:
     try:
         x = int(input())
-
+        for i in range(x):
+            for j in range(x):
+                print(i, end="")
+                print(j, "", end="")
+            print()
+        print(x // 3)
         for i in range(x):
             for j in range(x):
                 if i == j == ((x // 2)):
                     print(4, end="")
-                elif (x-1) // 3 <= i <= (2 * x) // 3 and ((x-1) // 3) <= j <= (2 * x) // 3:
+                elif (x) // 3 <= i < (2 * x) // 3 and ((x) // 3) <= j < (2 * x) // 3:
                     print(1, end="")
                 elif i == j:
                     print(2, end="")
