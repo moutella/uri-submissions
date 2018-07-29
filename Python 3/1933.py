42486a8e90ddb9fc544684afebbbbb28ba706bec
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:44:55 +0000

    Desafio 1933

---
+++ Python 3/1933.py
@@ -0,0 +1,11 @@
+#Nome: Tri
+#Resultado: Accepted
+#Data: 29/07/18 19:44:55
+#Linguagem: Python 3
+valores = input().split()
+a = int(valores[0])
+b = int(valores[1])
+if a == b:
+    print(a)
+else:
+    print(max(a, b))
