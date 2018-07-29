58c254ce84589347d50fc4246a29210aa27adeda
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 21:42:02 +0000

    Desafio 1963

---
+++ Python 3/1963.py
@@ -0,0 +1,11 @@
+#Nome: O Filme
+#Resultado: Runtime error
+#Data: 29/07/18 21:42:02
+#Linguagem: Python 3
+entradas = input().split()
+
+dif = int(entradas[1]) - int(entradas[0])
+
+percent = dif / int(entradas[0])
+percent *= 100
+print("{:.2f}%".format(percent))
