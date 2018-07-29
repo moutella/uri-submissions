d6314b1a0b1f8805446826a21998764ca83f059b
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 18:44:44 +0000

    Desafio 1847

---
+++ Python 3/1847.py
@@ -0,0 +1,13 @@
+#Nome: Bem
+#Resultado: Wrong answer (10%)
+#Data: 29/07/18 18:44:44
+#Linguagem: Python 3
+valores = input().split()
+a = int(valores[0])
+b = int(valores[1])
+c = int(valores[2])
+
+if(c-b > b-a):
+    print(":)")
+else:
+    print(":(")
