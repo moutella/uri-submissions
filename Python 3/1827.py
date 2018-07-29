cbc18e2b77b9a0bf04811bbc97c0c2b9f44903bb
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 16:49:07 +0000

    Desafio 1827

--- Python 3/1827.py
+++ Python 3/1827.py
@@ -1,20 +1,20 @@
 #Nome: Matriz Quadrada IV
-#Resultado: Wrong answer (20%)
-#Data: 29/07/18 16:33:24
+#Resultado: Wrong answer (10%)
+#Data: 29/07/18 16:49:07
 #Linguagem: Python 3
 while 1:
     try:
         x = int(input())
+
         for i in range(x):
             for j in range(x):
-                size = x-1
-                if(i==j==((x//2))):
+                if i == j == ((x // 2)):
                     print(4, end="")
-                elif(size//3 <= j <= 2*(size//3)+1) and (size // 3 <= i <= 2* (size // 3)+1):
+                elif (x-1) // 3 <= i <= (2 * x) // 3 and ((x-1) // 3) <= j <= (2 * x) // 3:
                     print(1, end="")
-                elif(i==j):
+                elif i == j:
                     print(2, end="")
-                elif(x-i == j+1 or x-j==i+1):
+                elif x - i == j + 1 or x - j == i + 1:
                     print(3, end="")
                 else:
                     print("0", end="")
