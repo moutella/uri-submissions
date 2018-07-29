cdc3be3afb5d92a284e73223f0ba0e7998ee9571
Moutella <joaogabrielmsc@gmail.com>
2018-07-29 19:25:53 +0000

    Desafio 1924

--- Python 3/1924.py
+++ Python 3/1924.py
@@ -1,5 +1,5 @@
 #Nome: Vitória e a Indecisão
-#Resultado: Runtime error
-#Data: 29/07/18 19:25:27
+#Resultado: Accepted
+#Data: 29/07/18 19:25:53
 #Linguagem: Python 3
-print("Ciencia da Computação")
+print("Ciencia da Computacao")
