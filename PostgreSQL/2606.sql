f1b7a214de9a37431134449d5752ef4930c4bf95
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 17:26:15 +0000

    Desafio 2606

---
+++ PostgreSQL/2606.sql
@@ -0,0 +1,8 @@
+--Nome: Categorias
+--Resultado: Accepted
+--Data: 12/05/19 17:26:15
+--Linguagem: PostgreSQL
+SELECT p.id, p.name from products as p
+where p.id_categories in(
+SELECT c.id from categories as c
+where c.name LIKE 'super%');
