c02ae4cf6baddfa28c3b08c01f11b6148d6d6d9c
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 18:05:25 +0000

    Desafio 2616

---
+++ PostgreSQL/2616.sql
@@ -0,0 +1,6 @@
+--Nome: Nenhuma Locação
+--Resultado: Accepted
+--Data: 12/05/19 18:05:25
+--Linguagem: PostgreSQL
+SELECT c.id, c.name from customers as c
+where c.id not in(select id_customers from locations);
