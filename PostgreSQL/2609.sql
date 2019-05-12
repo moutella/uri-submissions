ebbb22984134365c10d2f30c3336725ea92ec928
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 17:36:16 +0000

    Desafio 2609

---
+++ PostgreSQL/2609.sql
@@ -0,0 +1,7 @@
+--Nome: Produtos por Categorias
+--Resultado: Accepted
+--Data: 12/05/19 17:36:16
+--Linguagem: PostgreSQL
+select c.name, sum(amount) from products as p, categories c
+where p.id_categories = c.id
+GROUP BY c.name;
