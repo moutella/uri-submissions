1b82f1642af80b835c587b3d31b9c4f1c6bb2449
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 17:18:01 +0000

    Desafio 2605

---
+++ PostgreSQL/2605.sql
@@ -0,0 +1,7 @@
+--Nome: Representantes Executivos
+--Resultado: Accepted
+--Data: 12/05/19 17:18:01
+--Linguagem: PostgreSQL
+select p1.name, p2.name from products as p1
+join providers as p2 on p1.id_providers = p2.id
+where p1.id_categories = 6;
