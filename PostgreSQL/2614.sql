04c4d8137de79ead6a18cb4a11122ade7f61169c
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 18:00:00 +0000

    Desafio 2614

---
+++ PostgreSQL/2614.sql
@@ -0,0 +1,9 @@
+--Nome: Locações de Setembro
+--Resultado: Wrong answer (35%)
+--Data: 12/05/19 18:00:00
+--Linguagem: PostgreSQL
+select c.name, r.rentals_date from customers as c, rentals as r
+where r.id in(select r.id from rentals as r
+where EXTRACT(MONTH FROM r.rentals_date) = 9) and 
+c.id in(select r.id_customers from rentals as r
+where EXTRACT(MONTH FROM r.rentals_date) = 9);
