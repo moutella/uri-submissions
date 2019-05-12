4174fef28c6dd7204ac2aa71cc2748b1b97aca66
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 17:43:28 +0000

    Desafio 2611

---
+++ PostgreSQL/2611.sql
@@ -0,0 +1,8 @@
+--Nome: Filmes de Ação
+--Resultado: Accepted
+--Data: 12/05/19 17:43:28
+--Linguagem: PostgreSQL
+SELECT m.id, m.name from movies as m
+where m.id_genres in(
+select g.id from genres as g
+where g.description = 'Action');
