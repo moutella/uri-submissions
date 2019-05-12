57506e824e06560500530423b7f8545391f52f4c
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 17:49:44 +0000

    Desafio 2613

---
+++ PostgreSQL/2613.sql
@@ -0,0 +1,8 @@
+--Nome: Filmes em Promoção
+--Resultado: Accepted
+--Data: 12/05/19 17:49:44
+--Linguagem: PostgreSQL
+SELECT m.id, m.name from movies as m
+where m.id_prices in(
+	select p.id from prices as p
+	where p.value <2);
