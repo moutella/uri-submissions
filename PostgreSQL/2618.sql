7f1412effe1e368923d99d2ca1bb5f9335f19904
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 18:09:49 +0000

    Desafio 2618

---
+++ PostgreSQL/2618.sql
@@ -0,0 +1,10 @@
+--Nome: Produtos Importados
+--Resultado: Wrong answer (20%)
+--Data: 12/05/19 18:09:49
+--Linguagem: PostgreSQL
+SELECT p.name, p2.name from products as p
+join providers as p2 on(p.id_providers = p2.id)
+where p2.name = 'Sansul SA'
+and p.id_categories in (
+	select id from categories
+	where name = 'Imported');
