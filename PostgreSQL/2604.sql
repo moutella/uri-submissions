5ce19ea058031daed41fba039d96bae202527453
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 17:13:11 +0000

    Desafio 2604

---
+++ PostgreSQL/2604.sql
@@ -0,0 +1,6 @@
+--Nome: Menores que 10 ou Maiores que 100
+--Resultado: Accepted
+--Data: 12/05/19 17:13:11
+--Linguagem: PostgreSQL
+select id, name from products 
+where price < 10 or price > 100;
