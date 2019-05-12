c6b149a8cb2b50d8a70d94e370f750b2b90c7c04
Moutella <joaogabrielmsc@gmail.com>
2019-05-12 18:08:08 +0000

    Desafio 2617

---
+++ PostgreSQL/2617.sql
@@ -0,0 +1,7 @@
+--Nome: Fornecedor Ajax SA
+--Resultado: Accepted
+--Data: 12/05/19 18:08:08
+--Linguagem: PostgreSQL
+SELECT p.name, p2.name from products as p
+join providers as p2 on(p.id_providers = p2.id)
+where p2.name = 'Ajax SA';
